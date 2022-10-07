a94886ae211471260720a55178b88b39dde01bd5
Houssam almomani <houssamalmomani1@gmail.com>
2022-10-07 15:47:09 +0300

    complete expenses project

---
+++ constants/styles.js
@@ -0,0 +1,9 @@
+export const GlobalStyles = {
+  colors: {
+    primaryBG: "#f1faee",
+    primary1: "#05445E",
+    primary2: "#457b9d",
+    primary3: "#D4F1F4",
+    error500: "#e63946",
+  },
+};
