a3bfb8746ad3070fe3ed45b343addc1df3b20a2f
Houssam almomani <92177957+houssamalmomani@users.noreply.github.com>
2022-10-09 14:28:44 +0300

    Update http.js

--- util/http.js
+++ util/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL =
-    'https://expenses-tracker-593a0-default-rtdb.firebaseio.com';
+    '';
 
 export async function storeExpense(expenseData) {
     const response = await axios.post(BACKEND_URL + '/expenses.json', expenseData);
